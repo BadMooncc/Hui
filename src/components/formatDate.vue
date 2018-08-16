6140ea3798d9cab5e00b9cf9458927fd81a43af5
zhaodenghui <zhaodenghui@iyunxiao.com>
2018-08-16 11:12:10 +0800

    feat(demo): add demo code

--- src/components/formatDate.vue
+++ src/components/formatDate.vue
@@ -6,6 +6,10 @@
     <p>{{ 18003717997 | formatTel }}</p>
 
     <p>{{ 'today' | toUpperCase}}</p>
+
+    <p>{{ 410181199611296519 | formatIdCard }}</p>
+
+    <p>{{ '6217002430030521570' | formatBankCard }}</p>
   </div>
 </template>
 <script>
